33fc2a8a5ab1345de7cd081a1b8ee2ed92da10d1
nathan <liuyunathan@gmail.com>
2024-04-25 23:41:52 +0800

    update

--- src/utils/numbers.ts
+++ src/utils/numbers.ts
@@ -1,8 +1,8 @@
 import { BigNumber } from 'ethers'
 import JSBI from 'jsbi'
 import bn from 'bignumber.js'
-import { Percent } from '@uniswap/sdk-core'
-import { toHex } from '@uniswap/v3-sdk'
+import { Percent } from '@nathan2024/up-sdk-core'
+import { toHex } from '@nathan2024/up-v3-sdk'
 
 export function expandTo18DecimalsBN(n: number): BigNumber {
   // use bn intermediately to allow decimals in intermediate calculations
